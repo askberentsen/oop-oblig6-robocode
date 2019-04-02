cf265136e8f6a2b191f13a70ff17ac7fe325d09f
Ask Hetland Berentsen <askberentsen@gmail.com>
2019-04-02 15:26:16 +0200

    Added main method to ChampignonRobot

--- src/oop/gruppe4/robocode/robot/ChampignonRobot.java
+++ src/oop/gruppe4/robocode/robot/ChampignonRobot.java
@@ -2,5 +2,24 @@ package oop.gruppe4.robocode.robot;
 
 import robocode.AdvancedRobot;
 
+/*
+ * TODO: 02/04/2019 | ALL
+ *
+ *
+ */
+
+/**
+ * The main robot of the project.
+ * @author Gruppe 4.
+ */
 public class ChampignonRobot extends AdvancedRobot {
+
+    /**
+     * Main method of this {@code ChampignonRobot}.
+     */
+    @Override
+    public void run() {
+    }
+
+
 }
