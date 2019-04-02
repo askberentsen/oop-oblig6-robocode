b6921b90b4793562e9b5b20edf7ec5705e17332c
Ask Hetland Berentsen <askberentsen@gmail.com>
2019-04-02 15:59:02 +0200

    .gitignore fix (part deux)

---
+++ src/oop/gruppe4/robocode/robot/ChampignonRobot.java
@@ -0,0 +1,33 @@
+package oop.gruppe4.robocode.robot;
+
+import robocode.AdvancedRobot;
+import robocode.ScannedRobotEvent;
+
+/*
+ * TODO: 02/04/2019 | ALL
+ * TODO: Implement
+ * TODO: Add Yeet function
+ */
+
+/**
+ * The main robot of the project.
+ * @author Gruppe 4.
+ */
+public class ChampignonRobot extends AdvancedRobot {
+
+    /**
+     * Main method of this {@code ChampignonRobot}.
+     */
+    @Override
+    public void run() {
+    }
+
+    /**
+     * TODO 02/04/2019
+     * @param e
+     * @return
+     */
+    private double targetRobot(ScannedRobotEvent e){
+        return 0;
+    }
+}
