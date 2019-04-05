a99a12a6352b24fffc483c151e889d30e052c90b
Ask Hetland Berentsen <askberentsen@gmail.com>
2019-04-05 15:53:46 +0200

    Documented Transform

--- src/oop/gruppe4/robocode/transform/Transform.java
+++ src/oop/gruppe4/robocode/transform/Transform.java
@@ -1,20 +1,60 @@
 package oop.gruppe4.robocode.transform;
 
+/**
+ * A class representing the transform properties of an object.
+ * @author Ask Hetland Berentsen.
+ */
 public class Transform {
-    private Vector2 position, trajectory;
+
+    /**
+     * The position of an object.
+     */
+    private Vector2 position;
+
+    /**
+     * The trajectory of an object.
+     */
+    private Vector2 trajectory;
+
+    /**
+     * The velocity of an object.
+     */
     private double velocity;
+
+    /**
+     * Class constructor.
+     * @param x the x coordinate of an object.
+     * @param y the y coordinate of an object.
+     * @param dx the delta of {@code x}.
+     * @param dy the delta of {@code y}.
+     * @param velocity the velocity of an object.
+     */
     public Transform( double x, double y, double dx, double dy, double velocity){
         this.position = new Vector2(x,y);
         this.trajectory = new Vector2(dx, dy);
         this.velocity  = velocity;
     }
 
+    /**
+     * Gets the position.
+     * @return the coordinates of {@code this}.
+     */
     public Vector2 getPosition() {
         return position;
     }
+
+    /**
+     * Gets the trajectory.
+     * @return the vector {@code this} is moving in.
+     */
     public Vector2 getTrajectory() {
         return trajectory;
     }
+
+    /**
+     * Gets the velocity.
+     * @return the velocity of the trajectory.
+     */
     public double getVelocity() {
         return velocity;
     }
